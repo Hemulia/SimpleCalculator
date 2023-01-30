fe8ce2bb0d9be8d1b269164db9a25a6982010d94
Emilia Mäkelä <bgr887@myy.haaga-helia.fi>
2023-01-31 00:15:41 +0200

    Laskin historialla

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert, FlatList } from 'react-native';
 
 export default function App() {
-const [num1,setNum1] = useState(0);
-const [num2, setNum2] = useState(0); 
-const [result, setResult] = useState();
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0); 
+  const [result, setResult] = useState();
+  const [history, setHistory] = useState([]);
 
-const pressPlus = () => {
-  const plus = num1 + num2
-  setResult(plus)
-}
+  const pressPlus = () => {
+    const plus = num1 + num2;
+    setResult(plus);
+    setHistory([...history, `${num1} + ${num2} = ${plus}`]);
+  }
 
-const pressMinus = () => {
-  const minus = num1 - num2
-  setResult(minus)
-}
+  const pressMinus = () => {
+    const minus = num1 - num2;
+    setResult(minus);
+    setHistory([...history, `${num1} - ${num2} = ${minus}`]);
+  }
 
   return (
-    <View style={{flex:0.4,alignItems:'center', justifyContent:'space-evenly', top:100}}>
+    <View style={{flex:1, alignItems:'center', justifyContent:'space-evenly', top:100}}>
       <Text>Result: {result} </Text>
       <TextInput 
         type= "number" 
@@ -35,10 +38,15 @@ const pressMinus = () => {
         onChange={v => setNum2(Number.parseInt(v.nativeEvent.text))}
       />
       <View style={{flexDirection:'row', }}>
-      <Button title="+" onPress={pressPlus} />
-      <Button title="-" onPress={pressMinus}/>
+        <Button title="+" onPress={pressPlus} />
+        <Button title="-" onPress={pressMinus}/>
       </View>
+      <Text>History</Text>
+      <FlatList
+        data={history}
+        keyExtractor={item => history.indexOf(item).toString()}
+        renderItem={({ item }) => <Text>{item}</Text>}
+      />
     </View>
   );
 }
-
